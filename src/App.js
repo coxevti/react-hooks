7d39c664292c3f081c13a66c31d592d36922c374
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-23 06:14:37 -0400

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [techs, setTech] = useState(['React']);
+    const [techs, setTech] = useState([]);
     const [newTech, setNewTech] = useState('');
 
     function handleAddTech() {
         setTech([...techs, newTech]);
         setNewTech('');
     }
+
+    useEffect(() => {
+        const storageTechs = localStorage.getItem('techs');
+        if (storageTechs) {
+            setTech(JSON.parse(storageTechs));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techs', JSON.stringify(techs));
+    }, [techs]);
 
     return (
         <>
